e4bd97e702a368208adddf0ee0e9ca0c73da819b
Fredrik Svärd <fredrik.svard@svenskaspel.se>
2021-02-13 23:36:00 +0100

    first draft

---
+++ setup.py
@@ -0,0 +1,21 @@
+# setup.py
+from setuptools import find_packages, setup
+
+
+setup(
+    name='divby3',
+    version='0.0.1',
+    python_requires='>=3.6,<3.7',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 1 - Planning',
+
+        'Intended Audience :: Developers',
+
+        'Programming Language :: Python :: 3.6',
+    ],
+    author='Fredrik Svärd',
+    author_email='nfredrik@hotmail.com',
+    license='MIT',
+    url='https://github.com/nfredrik/divby3.git',
+)
